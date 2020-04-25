635cb966f85b52b200c75ed48a22f5f48be11ef4
xrenoder <60063923+xrenoder@users.noreply.github.com>
2020-04-26 02:54:04 +0300

    Abstract class Enum added. Classes RequestEnum, AliveReqRequest, AliveResRequest added. Request refactored to abstract class fix

--- classes/enums/RequestEnum.php
+++ classes/enums/RequestEnum.php
@@ -8,7 +8,7 @@ class RequestEnum extends Enum
     public const ALIVE_REQ = 1;
     public const ALIVE_RES = 2;
 
-    protected static $list = array(
+    protected static $items = array(
         0 => '',                                // empty for not using 0 type
         self::ALIVE_REQ => 'AliveReqRequest',
         self::ALIVE_RES => 'AliveResRequest',
