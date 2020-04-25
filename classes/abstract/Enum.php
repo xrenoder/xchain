d5854a6a8782559e08c65975cff38913e58812a3
xrenoder <60063923+xrenoder@users.noreply.github.com>
2020-04-26 02:52:39 +0300

    Abstract class Enum added. Classes RequestEnum, AliveReqRequest, AliveResRequest added. Request refactored to abstract class fix

--- classes/abstract/Enum.php
+++ classes/abstract/Enum.php
@@ -8,16 +8,16 @@ abstract class Enum
     protected static $items = array(/* override me */);
 
     public static function getItem($id) {
-        if (!isset(static::$items[$id])) {
+        if (!isset(self::$items[$id])) {
             throw new Exception(__CLASS__ . ' knows nothing about ' . $id);
         }
 
-        return static::$items[$id];
+        return self::$items[$id];
     }
 
     public static function isSet($id): bool
     {
-        if (isset(static::$items[$id])) {
+        if (isset(self::$items[$id])) {
             return true;
         }
 
@@ -26,6 +26,6 @@ abstract class Enum
 
     public static function getItemsList(): array
     {
-        return static::$items;
+        return self::$items;
     }
 }
