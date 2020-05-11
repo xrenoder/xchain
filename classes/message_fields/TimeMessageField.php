7fdd82acaa80b019be1b8455a6006ed38fb6a58e
xrenoder <60063923+xrenoder@users.noreply.github.com>
2020-05-11 23:29:35 +0300

    Refactoring: added classes of message fields

--- classes/message_fields/TimeMessageField.php
+++ classes/message_fields/TimeMessageField.php
@@ -8,16 +8,21 @@ class TimeMessageField extends aMessageField
 
     public function check(): bool
     {
-        $time = time() + 3;
+        // Clients (type NodeClassEnum::CLIENT_ID) may have unsynchronized time
+        if (
+            $this->getMessage()->getRemoteNodeId() === NodeClassEnum::CLIENT_ID
+            || $this->getMessage()->getSocket()->getMyNodeId() === NodeClassEnum::CLIENT_ID
+        ) {
+            return true;
+        }
+
+// TODO проверить ошибку времени при обмене сообщениями между неклиентскими нодами
+        $time = time();
         $diff = abs($this->value - $time);
 
 // TODO заменить 2 секунды разницы во времени между созданием запроса и приемом на константу или полученое из блокчейна правило
 // возможно, локальное время нужно брать в момент получения запроса
-        if (
-            $diff >= 2
-            && $this->getMessage()->getRemoteNodeId() !== NodeClassEnum::CLIENT_ID
-            && $this->getMessage()->getSocket()->getMyNodeId() !== NodeClassEnum::CLIENT_ID
-        ) {
+        if ($diff >= 2) {
             $this->dbg("BAD TIME message time $this->value have too big different with local time $time for " . $this->getMessage()->getName());
             $this->getMessage()->setBadTime();
             return false;
