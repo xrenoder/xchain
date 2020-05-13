a3c874e7fa82f338951c98c7902a4b2018b513a6
xrenoder <60063923+xrenoder@users.noreply.github.com>
2020-05-13 08:13:25 +0300

    Refactoring

--- classes/const/constDbRowIds.php
+++ classes/const/constDbRowIds.php
@@ -6,5 +6,5 @@ interface constDbRowIds
     public const LAST_KNOWN_BLOCK = 'lastKnownBlock';
     public const LAST_PREPARED_BLOCK = 'lastPreparedBlock';
     public const TRANSACTION_EMISSION_RULE = 'transactionEmissionRule';
-    public const TRANSACTION_REWARD_RULE = 'transactionRewardRule';
+    public const TRANSACTION_REWARDS_RULE = 'transactionRewardsRule';
 }
